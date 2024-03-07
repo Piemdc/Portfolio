cd8119011596f457ef1702486215f5c623247872
Pierre Magnien <pierre@opusdomus.fr>
2024-03-07 15:58:14 +0100

    remove pt on stacks wrapper mobile

--- Components/blocks/Stack.js
+++ Components/blocks/Stack.js
@@ -8,7 +8,7 @@ export default function Stack(){
                 <div className={"relative justify-end px-10 lg:grid grid-cols-2 min-h-screen  pt-[60px] lg:pt-0"}>
                     <div className={"text-3xl self-center lg:p-10 text-left relative uppercase lg:col-span-1"}>
                         <h2 className={"font-title font-extralight hover:text-secondary-focus transition-colors duration-300 capitalize text-6xl mb-10 lg:text-right"}>Stack technique</h2>
-                        <div className={'flex flex-col items-end w-full pt-[100px] lg:pt-0'}>
+                        <div className={'flex flex-col items-end w-full'}>
                             <StackWrapper/>
                         </div>
                     </div>
