e060def8d7f08b883b59ebfafd8a8d4b84ebeb0d
Pierre Magnien <pierre@opusdomus.fr>
2024-03-07 12:09:10 +0100

    divers fixs

--- Components/atoms/RealBlock.js
+++ Components/atoms/RealBlock.js
@@ -1,7 +1,7 @@
 
 export default function RealBlock(props){
     return (
-        <a href={props.url} className={'w-[40%] lg:w-[20%] flex flex-col justify-center'}>
+        <a href={props.url} className={'border-[1px] border-black/10 h-[140px] bg-black/25 transition duration-700 hover:bg-black/75 hover:scale-90 backdrop-blur rounded-xl p-4 w-[40%] lg:w-[20%] flex flex-col justify-center'}>
             <div className={'svg__wrapper'}>{props.image}</div>
         </a>
     )
