e1a3c60446e66d4baa23aec664fefd70050d53e0
Pierre Magnien <pierre@opusdomus.fr>
2024-03-07 15:56:38 +0100

    min-h-screen on blocks

--- Components/blocks/Hero.js
+++ Components/blocks/Hero.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Hero(){
 
     return (
-        <section id={"hero"} className={"px-10 pt-[60px] lg:pt-0 w-full text-primary-content" }>
+        <section id={"hero"} className={"px-10 pt-[60px] lg:pt-0 w-full min-h-screen text-primary-content" }>
             <div className={"lg:grid grid-cols-2 lg:min-h-screen font-sans uppercase pt-[60px] lg:pt-0"}>
                 <div className={"lg:col-span-1 self-center font-black tracking-tighter lg:text-right mb-10 lg:mb-0 lg:pr-10 lg:border-r-2 relative"}>
                     <span className={"absolute lowercase font-title top-[-1em] lg:right-10 text-secondary-focus lg:text-4xl"}>Junior</span>
--- Components/blocks/Stack.js
+++ Components/blocks/Stack.js
@@ -4,7 +4,7 @@ import StackWrapper from "@/Components/Molecules/Stack-wrapper";
 
 export default function Stack(){
     return (
-            <section id={"stack"} className={"z-20 py-[60px] lg:py-0 w-full text-primary-content relative"}>
+            <section id={"stack"} className={"z-20 py-[60px] min-h-screen lg:py-0 w-full text-primary-content relative"}>
                 <div className={"relative justify-end px-10 lg:grid grid-cols-2 min-h-screen  pt-[60px] lg:pt-0"}>
                     <div className={"text-3xl self-center lg:p-10 text-left relative uppercase lg:col-span-1"}>
                         <h2 className={"font-title font-extralight hover:text-secondary-focus transition-colors duration-300 capitalize text-6xl mb-10 lg:text-right"}>Stack technique</h2>
