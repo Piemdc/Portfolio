1bf92162a45711fc036e9a1dede0fd68244f97c1
Pierre Magnien <pierre@opusdomus.fr>
2024-03-07 15:40:03 +0100

    bg to top

--- Components/atoms/BgBody.js
+++ Components/atoms/BgBody.js
@@ -6,16 +6,16 @@ export default function BgBody() {
     const currentIndex = useContext(ScrollPositionContext)
     console.log(currentIndex)
     return(
-        <div id={'bgbody'} className={'fixed w-full'}>
-            <div className={'hero w-full min-h-lvh absolute opacity-1 transition-opacity duration-100 ease-in-out'}></div>
-            <div className={'about bg-[60%] lg:bg-center w-full min-h-lvh absolute opacity-0 transition-opacity'} style={{
+        <div id={'bgbody'} className={'fixed top-0 h-lvh w-full'}>
+            <div className={'hero w-full h-full absolute opacity-1 transition-opacity duration-100 ease-in-out'}></div>
+            <div className={'about bg-[60%] lg:bg-center w-full h-full absolute opacity-0 transition-opacity'} style={{
                 opacity: currentIndex.index === 0 && currentIndex.percent > 0
                     ? Number((currentIndex.percent / 100).toFixed(1))
                     : currentIndex.index === 1 && currentIndex.percent >= 0
                         ? 1 -Number((currentIndex.percent / 100).toFixed(1))
                         : 0
             }}></div>
-            <div className={'stack bg-[60%] lg:bg-center w-full min-h-lvh absolute opacity-0 transition-opacity'} style={{
+            <div className={'stack bg-[60%] lg:bg-center w-full h-full absolute opacity-0 transition-opacity'} style={{
                 opacity: currentIndex.index === 1 && currentIndex.percent > 0.20
                     ? Number((currentIndex.percent / 100).toFixed(1))
                     : currentIndex.index >= 2
