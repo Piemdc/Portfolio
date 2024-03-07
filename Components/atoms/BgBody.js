221053f5235b07e94a1de93cc9a41c13692f7957
Pierre Magnien <pierre@opusdomus.fr>
2024-03-07 15:16:24 +0100

    change bg on stack

--- Components/atoms/BgBody.js
+++ Components/atoms/BgBody.js
@@ -4,13 +4,21 @@ import { useContext } from "react";
 
 export default function BgBody() {
     const currentIndex = useContext(ScrollPositionContext)
+    console.log(currentIndex)
     return(
         <div id={'bgbody'} className={'fixed w-full'}>
             <div className={'hero w-full min-h-screen absolute opacity-1 transition-opacity duration-100 ease-in-out'}></div>
             <div className={'about bg-[60%] lg:bg-center w-full min-h-screen absolute opacity-0 transition-opacity'} style={{
                 opacity: currentIndex.index === 0 && currentIndex.percent > 0
                     ? Number((currentIndex.percent / 100).toFixed(1))
-                    : currentIndex.index >= 1
+                    : currentIndex.index === 1 && currentIndex.percent >= 0
+                        ? 1 -Number((currentIndex.percent / 100).toFixed(1))
+                        : 0
+            }}></div>
+            <div className={'stack bg-[60%] lg:bg-center w-full min-h-screen absolute opacity-0 transition-opacity'} style={{
+                opacity: currentIndex.index === 1 && currentIndex.percent > 0.20
+                    ? Number((currentIndex.percent / 100).toFixed(1))
+                    : currentIndex.index >= 2
                         ? 1
                         : 0
             }}></div>
